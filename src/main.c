d7682c4c7c25406fdd06f9a443eb03bf234d172e
Dado Mista <dadomista@gmail.com>
2024-12-27 02:29:14 -0800

    External apps tuning support
    -Support extended range of some values for tuning from external apps

--- src/main.c
+++ src/main.c
@@ -1898,8 +1898,10 @@ static void cmd_runtime_tune(data *d, unsigned char *cfg, int len) {
 
         split(cfg[7], &h1, &h2);
         d->float_conf.atr_angle_limit = h1 + 5;
-        d->float_conf.atr_on_speed = (h2 & 0x3) + 3;
-        d->float_conf.atr_off_speed = (h2 >> 2) + 2;
+        if (h2 > 0) {
+            d->float_conf.atr_on_speed = (h2 & 0x3) + 3;
+            d->float_conf.atr_off_speed = (h2 >> 2) + 2;
+        }
 
         split(cfg[8], &h1, &h2);
         d->float_conf.atr_response_boost = ((float) h1) / 10 + 1;
@@ -1944,7 +1946,7 @@ static void cmd_runtime_tune(data *d, unsigned char *cfg, int len) {
         split(cfg[15], &h1, &h2);
         float onspd = h1;
         float offspd = h2;
-        d->float_conf.torquetilt_on_speed = onspd / 2;
+        d->float_conf.torquetilt_on_speed = onspd + 3;
         d->float_conf.torquetilt_off_speed = offspd + 3;
     }
     if (len >= 17) {
@@ -1952,6 +1954,17 @@ static void cmd_runtime_tune(data *d, unsigned char *cfg, int len) {
         d->float_conf.kp_brake = ((float) h1 + 1) / 10;
         d->float_conf.kp2_brake = ((float) h2) / 10;
         beep_alert(d, 1, 1);
+    }
+    if (len >= 19) {
+        split(cfg[17], &h1, &h2);
+        if (h1 > 0) {
+            d->float_conf.mahony_kp_roll = ((float) h1) / 10 + 1.0;
+        }
+        split(cfg[18], &h1, &h2);
+        if ((h1 > 0) && (h2 > 0)) {
+            d->float_conf.atr_on_speed = h1 * 2;
+            d->float_conf.atr_off_speed = h2 * 2;
+        }
     }
 
     reconfigure(d);
@@ -2397,7 +2410,7 @@ static void on_command_received(unsigned char *buffer, unsigned int len) {
         send_buffer[ind++] = 101;  // magic nr.
         send_buffer[ind++] = 0x0;  // command ID
         send_buffer[ind++] = (uint8_t) (10 * PACKAGE_MAJOR_MINOR_VERSION);
-        send_buffer[ind++] = 1;  // build number
+        send_buffer[ind++] = 2;  // build number
         // Send the full type here. This is redundant with cmd_light_info. It
         // likely shouldn't be here, as the type can be reconfigured and the
         // app would need to reconnect to pick up the change from this command.
